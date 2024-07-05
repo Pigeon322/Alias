d702f47824168775db5111058d579035411c2c4b
Pigeon322 <guffenok@yandex.ru>
2024-07-05 17:41:01 +0300

    Почти сделал вью категории

--- Alias/CategoryView/CategoriesView.swift
+++ Alias/CategoryView/CategoriesView.swift
@@ -12,18 +12,24 @@ struct CategoriesView: View {
     @StateObject private var viewModel = CategoriesViewModel()
         
     var body: some View {
-        VStack {
+        VStack(spacing: 15) {
             PlainLabel(text: "Выберите наборы слов")
             
-            CategoryView(category: Category(name: "Базовый набор", icon: "checkmark.circle.fill", words: ["Был","Ты","Человек"]))
+            CategoryView(category: Category(name: "Базовый набор", icon: "booksCategory", words: ["Был","Бы","Ты","Человек"]))
+                .padding(.horizontal, 20)
         }
+        .frame(maxWidth: .infinity)
+        .frame(maxHeight: .infinity)
+        .background(.black)
     }
     
     private var categoriesView: some View {
         ScrollView {
             LazyVStack {
                 Section {
-                    
+                    ForEach(viewModel.category, id: \.name) {
+                        CategoryView(category: $0)
+                    }
                 }
             }
         }
--- Alias/UI/CategoryView.swift
+++ Alias/UI/CategoryView.swift
@@ -11,21 +11,35 @@ struct CategoryView: View {
     let category: Category
     
     var body: some View {
-        VStack {
-            Text(category.name)
-                .bold()
-                .font(.system(size: 15))
-                .foregroundStyle(.white)
-            
-            Text(category.words.count.description)
-                .bold()
-                .font(.system(size: 10))
-                .foregroundStyle(.white)
-                .background(.blue)
-                .cornerRadius(15)
+        ZStack {
+            VStack {
+                Spacer()
+                
+                Text(category.name)
+                    .bold()
+                    .font(.system(size: 20))
+                    .foregroundStyle(.white)
+                
+                Spacer()
+                
+                Text(category.words.count.description)
+                    .frame(width: 100)
+                    .frame(height: 25)
+                    .bold()
+                    .font(.system(size: 15))
+                    .foregroundStyle(.white)
+                    .background(.blue)
+                    .cornerRadius(15)
+                    .padding(.bottom, 15)
+            }
         }
         .frame(maxWidth: .infinity)
-        .background(Image("checkmark.circle.fill")) // TODO: - Временно, так должно быть Image(category.icon)
-        .border(.white)
+        .frame(height: 150)
+        .background(Image(category.icon))
+        .cornerRadius(15)
+        .overlay {
+            RoundedRectangle(cornerRadius: 15)
+                 .stroke(.white, lineWidth: 5)
+        }
     }
 }
